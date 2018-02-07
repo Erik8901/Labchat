2c3b751234fc731bfc01d1ba5e4c1eba7837ab0c
erik8901 <erikjohansson890@gmail.com git config --global user.name erik8901git config --global user.email erikjohansson890@gmail.com>
2018-02-07 11:16:14 +0100

    changes labchat

---
+++ js/authGit.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", auth)
+
+function auth() {
+        let provider = new firebase.auth.GithubAuthProvider();
+        let btnGit = document.getElementById("login");
+    
+    
+        btnGit.addEventListener("click", function(popUp) {
+            
+            
+            
+            
+            
+            
+            
+            
+        })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+};
